183f5586d27f8f2f0fd9f9167e3813570d2af3e4
endritmorina94 <endrit.morina@outlook.it>
2021-03-29 15:52:57 +0200

    finito js

--- js/script.js
+++ js/script.js
@@ -12,5 +12,35 @@ generateButton.addEventListener("click", function () {
     slider.className = slider.classList + " darker";
     ticketGenerator.className = ticketGenerator.classList + " darker";
 
+    var prezzoStandard = 1000;
+
+    //Dichiaro delle varibili contententi il valore dei miei input
+    var userName = document.getElementById("nome").value;
+    var userTime = parseInt(document.getElementById("anni-residenza").value);
+    var userAge = document.getElementById("age-user").value;
+    var userClass = parseInt(document.getElementById("class-user").value);
+    var userOffer = "Prezzo standard";
+
+
+    // Dichiaro il prezzo finale con la sua formula
+    var finalPrice = prezzoStandard * userTime * userClass;
+
+    if (userAge == "minorenne"){
+        userOffer = "Sconto minorenni";
+        finalPrice * 0.8;
+    } else if (userAge == "over") {
+        userOffer = "Sconto over 65";
+        finalPrice * 0.6;
+    }
+
+    var userCode = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+    var userWagon = Math.floor(Math.random() * (1002 - 1) ) + 1;
+
+    document.getElementById("user-name").innerHTML = userName;
+    document.getElementById("user-code").innerHTML = userCode;
+    document.getElementById("user-wagon").innerHTML = userWagon;
+    document.getElementById("user-offer").innerHTML = userOffer;
+
+    document.getElementById("final-price").innerHTML = finalPrice;
 
 });
