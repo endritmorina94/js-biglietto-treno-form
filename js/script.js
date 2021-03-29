03751969155e7251843fec9f471533f138bac7de
endritmorina94 <endrit.morina@outlook.it>
2021-03-29 15:17:58 +0200

    impostate prima variabili

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+// imposto una variabile per tutti gl'elementi che mi servono
 var generateButton = document.getElementById("generate-button");
+var checkout = document.getElementById("checkout");
+var slider = document.getElementById("slider");
+var ticketGenerator = document.getElementById("ticket-generator");
 
+// Al click del bottone "genera"....
 generateButton.addEventListener("click", function () {
-    alert("ciao");
+    // Aggiungo la classe left per mostrare il carrello all'utente
+    checkout.className = "left";
+    // Aggiungo la classe darker per oscurare il resto della schermata
+    slider.className = slider.classList + " darker";
+    ticketGenerator.className = ticketGenerator.classList + " darker";
+
+
 });
